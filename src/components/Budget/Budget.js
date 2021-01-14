e2bb7b84b28cb9fc8d82b4edaded25019dd62493
keplerridge <kepler.ridge@gmail.com>
2021-01-13 17:01:09 -0700

    one step left in project

--- src/components/Budget/Budget.js
+++ src/components/Budget/Budget.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
-import {requestUserData} from './ducks/userReducer';
+import {requestUserData} from './../../ducks/userReducer';
+import {requestBudgetData} from './../../ducks/budgetReducer';
 import Background from './../shared/Background/Background'
 import Chart1 from './../shared/Chart1';
 import Chart2 from './../shared/Chart2';
@@ -14,23 +15,25 @@ import './Budget.css';
 class Budget extends Component {
   componentDidMount(){
     this.props.requestUserData();
+    this.props.requestBudgetData();
   }
 
   render() {
-    const {loading} = this.props.budget;
+    const {loading, purchases, budgetLimit} = this.props.budget;
+    const {firstName, lastName} = this.props.user;
     return (
       <Background>
         {loading ? <Loading /> : null}
         <div className='budget-container'>
-          <Nav />
+          <Nav firstName={firstName} lastName={lastName}/>
           <div className='content-container'>
             <div className="purchases-container">
               <AddPurchase />
-              <DisplayPurchases />
+              <DisplayPurchases purchases={purchases}/>
             </div>
             <div className='chart-container'>
-              <Chart1 />
-              <Chart2 />
+              <Chart1 purchases={purchases} budgetLimit={budgetLimit}/>
+              <Chart2 purchases={purchases}/>
             </div>
           </div>
         </div>
@@ -39,6 +42,6 @@ class Budget extends Component {
   }
 }
 
-const mapStateToProps = state => ({budget: state.budget})
+const mapStateToProps = state => ({budget: state.budget, user: state.user})
 
-export default connect(mapStateToProps ,{requestUserData})(Budget);
+export default connect(mapStateToProps ,{requestUserData, requestBudgetData})(Budget);
